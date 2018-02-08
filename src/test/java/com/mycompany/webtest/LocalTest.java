79c2f0baa65cad6d0c89c0ae427281d71726036e
bogdan.zastavnyi <bogdan.zastavnyi@irdeto.com>
2018-02-08 12:03:44 +0100

    disabled firefox test

--- src/test/java/com/mycompany/webtest/LocalTest.java
+++ src/test/java/com/mycompany/webtest/LocalTest.java
@@ -14,7 +14,7 @@ public class LocalTest {
     private WebDriver driver;
 
     @Test
-    public void testChrome() throws InterruptedException {
+    public void testChrome() {
         if (OsCheck.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
         }
@@ -26,7 +26,7 @@ public class LocalTest {
         Assert.assertEquals(driver.getTitle(), "Selenium Test Example Page");
     }
 
-    @Test
+    @Test(enabled = false)
     public void testFirefox() {
         driver = new FirefoxDriver(); //for firefox you don't have to specify driver file, it's built into Firefox browser
         driver.get("http://crossbrowsertesting.github.io/selenium_example_page.html");
